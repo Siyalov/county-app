dac25c082edb6729c0e2e60a9f676e3204653d81
Siyalov <91375207+Siyalov@users.noreply.github.com>
2023-02-16 18:26:58 +0200

    router update

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { path } from "./settings";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={path}>
     <App />
   </BrowserRouter>
 );
---
+++ src/settings.ts
@@ -0,0 +1,1 @@
+export const path = process.env.REACT_APP_PUBLIC_ROOT || window.location.href.includes('github.') ? '/county-app/' : '/';
