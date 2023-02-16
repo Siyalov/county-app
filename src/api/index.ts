65702f0db9ab3a82035a51423842cdacc49accba
Siyalov <91375207+Siyalov@users.noreply.github.com>
2023-02-16 14:43:17 +0200

    api update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,19 @@
 import { Countries } from "./types";
 
-const apiURL = 'https://restcountries.com/v3.1/all';
+const apiURL = 'https://restcountries.com/v3.1/';
 
 class API {
   async getCountries() {
-    const response = await fetch(apiURL);
+    const response = await fetch(apiURL + 'all');
+    if (response.ok) {
+      const data = await response.json();
+      return data as Array<Countries>;
+    } else {
+      return [];
+    }
+  }
+  async getCountriesByCCA3(cca3: string) {
+    const response = await fetch(apiURL + 'alpha/' + cca3);
     if (response.ok) {
       const data = await response.json();
       return data as Array<Countries>;
