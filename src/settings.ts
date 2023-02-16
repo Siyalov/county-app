2ca6e2a3570e285093c053b4d14e4b92c2846687
Siyalov <91375207+Siyalov@users.noreply.github.com>
2023-02-16 18:35:37 +0200

    Public root update

--- src/settings.ts
+++ src/settings.ts
@@ -1,1 +1,5 @@
-export const path = process.env.REACT_APP_PUBLIC_ROOT || window.location.href.includes('github.') ? '/county-app/' : '/';
+// do not forget also set PUBLIC_URL in .env
+export const path =
+  process.env.REACT_APP_PUBLIC_ROOT || window.location.href.includes("github.")
+    ? "/county-app/"
+    : "/";
